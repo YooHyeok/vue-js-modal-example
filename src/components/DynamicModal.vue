e1cd5eb44b12aa061f82da190749cf84a0aac031
webdevyoo <yjk7454@naver.com>
2024-12-17 06:02:34 +0900

    feat: `DynamicModal.vue` - 동적 모달 컴포넌트 예제 생성

---
+++ src/components/DynamicModal.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+      A new paragraph will be added every 5 sec to show how
+          <b>height</b> scales.
+    </div>
+</template>
+<script>
+/* eslint-disable vue/no-unused-components */
+export default {
+  name: 'ModalExample',
+}
+</script>
+<style>
+
+</style>
