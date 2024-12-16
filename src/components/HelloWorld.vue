88ac6314d0fd51b1c746465a2f8581ab94c3a589
webdevyoo <yjk7454@naver.com>
2024-12-17 06:06:19 +0900

    feat: `HellowWord.vue` - DynamicModal.vue 컴포넌트 vue-js-modal 동적 모달 호출

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,50 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <button class="btn green" @click="show">Fade Modal</button>
   </div>
 </template>
 
 <script>
+import DynamicModal from './DynamicModal.vue';
 export default {
   name: 'HelloWorld',
+  components: {
+    DynamicModal
+  },
   props: {
     msg: String
+  },
+  methods: {
+    show() {
+      this.$modal.show(DynamicModal, {
+        overlayTransition: 'vm-transition--overlay',
+        transition: "vm-transition--modal"
+      })
+    }
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+button.btn.green {
+    box-shadow: 0 4px 8px rgba(80, 201, 186, .3);
+    background: #50c9ba;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+button.btn {
+    outline: none;
+    background: #fff;
+    border: 0;
+    padding: 10px 18px;
+    cursor: pointer;
+    color: #fff;
+    box-shadow: 0 4px 8px rgba(32, 160, 255, .3);
+    background: #4db3ff;
+    font-weight: 600;
+    border-radius: 3px;
+    min-width: 90px;
+    margin-bottom: 8px;
+    margin-top: 8px;
+    margin-right: 8px;
 }
 </style>
